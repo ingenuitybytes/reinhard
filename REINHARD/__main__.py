c2388cfbd5a6108fbb816b25f47ce3df540847de
Alois Zimmermann <alois.zimmermann2003@gmail.com>
2022-10-22 19:38:50 +0200

    Update to discord.py@2.0.1

--- REINHARD/__main__.py
+++ REINHARD/__main__.py
@@ -2,38 +2,36 @@ import os
 from itertools import cycle
 
 import discord
-from discord import app_commands
 from discord.ext import commands, tasks
 
 from REINHARD.additionals.data import *
 
-game = cycle(['.help', '/help'])
-
-reinhard = commands.Bot(command_prefix=".", intents=discord.Intents.all())
-
-class reinhard(discord.Client):
+class MyBot(commands.Bot):
     def __init__(self):
-        super().__init__(intents=discord.Intents.default())
-        self.synced = False;
-        
+        super().__init__(
+            command_prefix='.', 
+            intents=discord.Intents.all(),
+            application_id=APPLICATION_ID
+            )
+
+    async def setup_hook(self):
+        for filename in os.listdir("REINHARD/cogs"):
+            if filename.endswith(".py") and filename != "__init__.py":
+                await self.load_extension(f"REINHARD.cogs.{filename[:-3]}")
+                await bot.tree.sync(guild=discord.Object(id=826868484138598400))
+
+
     async def on_ready(self):
-        await self.wait_until_ready()
-        if not self.synced:
-            await self.tree.sync(guild = discord.Object(id = 488677373835870208))
-            self.synced = True
+        print('--------------------')
+        log.info('Logged in as {}'.format(self.user.name))
+        log.info('ID: {}'.format(self.user.id))
+        print('--------------------')
 
-            print('--------------------')
-            log.info('Logged in as {}'.format(self.user.name))
-            log.info('ID: {}'.format(self.user.id))
-            print('--------------------') 
-            change_status.start()
-                
-            @tasks.loop(seconds=5) 
-            async def change_status():
-                await self.change_presence(activity=discord.Game(next(game)), status=discord.Status.online)
+        game = cycle(['/help', '/new'])
+        @tasks.loop(seconds=5) 
+        async def change_status():
+            await self.change_presence(activity=discord.Game(next(game)), status=discord.Status.online)
+        change_status.start()
 
-            for filename in os.listdir("REINHARD/cogs"):
-                if filename.endswith(".py") and filename != "__init__.py":
-                    self.load_extension(f"REINHARD.cogs.{filename[:-3]}") 
-                    
-reinhard.run(TOKEN)
+bot = MyBot()
+bot.run(TOKEN)
